c3dd72a6f36dc851df07ada145f60673f6003642
jruizr <jruizr@DESKTOP-R2VNJHG>
2022-03-01 20:17:23 +0100

    Linea 1

--- actividad4/src/AleatorioEmple.java
+++ actividad4/src/AleatorioEmple.java
@@ -7,7 +7,7 @@ public class AleatorioEmple {
 	public static void main(String[] args) throws IOException {
 		crear();
 		leer();
-		System.out.println("Esta línea no sirve");
+		System.out.println("Esta línea no sirve para nada");
 	}
 
 	public static void leer() throws IOException {
